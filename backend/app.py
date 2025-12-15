cd36edc97ccc745b5c36446c397c82f566d5aa17
Nicolas <nicolasdelfino585@gmail.com>
2025-12-15 17:26:21 -0300

    reescribiendo rutas hasta put en proceso

---
+++ backend/app.py
@@ -0,0 +1,120 @@
+from flask import Flask, jsonify, request
+import psycopg2
+
+app = Flask(__name__)
+
+
+def get_connection():
+    return psycopg2.connect(
+        host='localhost',
+        database='nico_fullsatck',
+        user='postgres',
+        password='1234'
+    )
+
+def fila_a_dict(fila):
+    if fila is None:
+        return None
+    
+    producto = {
+        "id" : fila[0],
+        "nombre" : fila[1],
+        "precio" : float(fila[2]),
+        "stock" : fila[3],
+    }
+
+    return producto
+
+@app.route('/productos', methods=['GET'])
+def get_productos():
+    conexion = get_connection()
+    cursor = conexion.cursor()
+
+    consulta = 'SELECT nombre, precio, stock FROM productos;'
+
+    cursor.execute(consulta)
+
+    filas = cursor.fetchall()
+
+    cursor.close()
+    conexion.close()
+
+    resultado = []
+
+    for fila in filas:
+        resultado.append(fila_a_dict(fila))
+    return jsonify(resultado), 200
+    
+
+@app.route('/productos/<int:producto_id>', methods=['GET'])
+def get_por_id(producto_id):
+    conexion = get_connection()
+    cursor = conexion.cursor()
+
+    consulta = 'SELECT id, nombre, precio, stock FROM productos WHERE id = %s;'
+
+    cursor.execute(consulta,(producto_id,))
+
+    fila = cursor.fetchone()
+
+    cursor.close()
+    conexion.close()
+
+
+    if fila is None:
+        jsonify({'error': 'producto no encontrado'}), 404
+    return jsonify(fila_a_dict(fila)) 
+
+@app.route('/productos', methods=['POST'])
+def crear_producto():
+    datos = request.get_json()
+
+    nombre = datos.get('nombre')
+    precio = datos.get('pecio')
+    stock = datos.get('stock')
+
+    if nombre is None or precio is None or stock is None:
+        return jsonify({'error': ' faltan datos obligatorios'}), 400
+    
+    conexion = get_connection()
+    cursor = conexion.cursor()
+
+    consulta = """
+        INSERT INTO productos(nombre, precio, stock)
+        VALUE (%s,%s,%s)
+        RETURNING id, nombre, precio, stock;
+        """
+    cursor.execute(consulta,(nombre,precio,stock))
+
+    fila = cursor.fetchone()
+
+    conexion.commit()
+
+    cursor.close()
+    conexion.close()
+
+    return jsonify(fila_a_dict(fila)), 201
+
+@app.route('/productos/<int:producto_id>', methods=['PUT'])
+def editar_producto(producto_id):
+    datos = request.get_json()
+
+    nombre = datos.get('nombre')
+    precio = datos.get('pecio')
+    stock = datos.get('stock')
+
+    if nombre is None or precio is None or stock is None:
+        return jsonify({'error': 'faltan datos obligatorios'}), 400
+    
+    conexion = get_connection()
+    cursor = conexion.cursor()
+
+    consulta_existe = 'SELECT id FROM producto WHERE id = %s;'
+    cursor.execute(consulta_existe,(producto_id,))
+
+    fila_existe = cursor.fetchone()
+
+    if fila_existe is None:
+        return jsonify({'error': 'producto no encontrado'}), 404
+    
+    consulta_upadte = 'UPDATE '
--- backend/apps.py
+++ backend/apps.py
@@ -1,139 +1,3 @@
-"""from flask import Flask, jsonify, request
-import psycopg2
-
-
-app = Flask(__name__)
-
-def get_db_conection():
-    return psycopg2.connect(
-        host="localhost",
-        database="nico_db",
-        user="nico",
-        password="1234"
-    )
-
-def fila_a_producto_dict(fila):
-    if fila is None:
-        return None
-    
-    producto = {
-        "id": fila[0],
-        "nombre": fila[1],
-        "precio": float(fila[2]),
-        "stock": fila[3]
-    }
-    return producto
-
-
-@app.route("/productos", methods=['GET'])
-def get_productos():
-    conexion = get_db_conection()
-    cursor = conexion.cursor()
-
-    consulta = 'SELECT id, nombre, precio, stock FROM productos;'
-    cursor.execute(consulta)
-
-    resultados = cursor.fetchall()
-
-    cursor.close()
-    conexion.close()
-
-    lista = []
-    for fila in resultados:
-        producto_dict = fila_a_producto_dict(fila)
-
-        lista.append(producto_dict)
-    return jsonify(lista), 200
-
-
-@app.route("/productos/<int:producto_id>", methods=["GET"])
-def obtender_producto_por_id(producto_id):
-    conexion = get_db_conection()
-    cursor = conexion.cursor()
-
-    consulta_sql = 'SELECT id, nombre, precio, stock FROM productos WHERE id = %s;'
-
-    cursor.execute(consulta_sql, (producto_id,))
-
-    resultado = cursor.fetchone()
-
-    cursor.close()
-    conexion.close()
-
-    if resultado is None:
-        return jsonify({"error": "producto no encontrado"}), 404
-    
-    producto_dict = fila_a_producto_dict(resultado)
-
-    return jsonify(producto_dict), 200
-##########################################"""
-
-"""from flask import Flask, jsonify, request
-import psycopg2
-
-
-app = Flask(__name__)
-
-def get_conection():
-    return psycopg2.connect(
-        host="localhost",
-        database="fullstack_db",
-        user="nico",
-        password="1234"
-    )
-
-#funcion auxiliar connvierte resultado sql a diccionario python
-
-def fila_a_dict(fila):
-    if fila is None:
-        return None
-    
-    producto = {
-        "id": fila[0],
-        "nombre": fila[1],
-        "precio": float(fila[2]),
-        "stock": fila[3]
-    }
-
-    return producto
-
-@app.route("/productos", methods=["GET"])
-def get_productos():
-    conexion = get_conection()
-    cursor = conexion.cursor()
-
-    consulta = 'SELECT id, nombre, precio, stock FROM productos;'
-    cursor.execute(consulta)
-
-    filas = cursor.fetchall()
-
-    cursor.close()
-    conexion.close()
-
-    resultado = []
-    for fila in filas:
-        resultado.append(fila_a_dict(fila))
-    return jsonify(resultado), 200
-
-@app.route("/productos/<int:producto_id>", methods=["GET"])
-def get_producto_id(producto_id):
-    conexion = get_conection()
-    cursor = conexion.cursor()
-
-    consulta = 'SELECT id, nombre, precio, stock FROM productos WHERE id = %s'
-    cursor.execute(consulta, (producto_id,))
-
-    fila = cursor.fetchone()
-
-    cursor.close()
-    conexion.close()
-
-    if fila is None:
-        return jsonify({"error":"producto no encontrado"}), 404
-
-    return jsonify(fila_a_dict(fila)), 200
-"""
-#############################################################3
 from flask import Flask, jsonify, request
 import psycopg2
 
@@ -203,6 +67,8 @@ def crear_producto():
     precio = datos.get('precio')
     stock = datos.get('stock')
 
+    #######################################
+
     if nombre is None or precio is None or stock is None:
         return jsonify({"error": 'falta alguno de los campos obligatorios'}), 400
     
@@ -225,6 +91,75 @@ def crear_producto():
 
     return jsonify(fila_a_dict(fila)), 201
 
+@apps.route('/productos/<int:producto_id>', methods=['PUT'])
+def editar_producto(producto_id):
+    datos = request.get_json()
+
+    nombre = datos.get('nombre')
+    precio = datos.get('precio')
+    stock = datos.get('stock')
+
+    #######################################
+
+    if nombre is None or precio is None or stock is None:
+        return jsonify({'error': 'falta un campo obligatorio'}), 400
+    
+    conexion = get_connection()
+    cursor = conexion.cursor()
+
+    consulta_existe = 'SELECT id, nombre, precio, stock FROM productos WHERE id = %s;'
+
+    cursor.execute(consulta_existe,(producto_id,))
+    fila_existe = cursor.fetchone()
+
+    if fila_existe is None:
+        cursor.close()
+        conexion.close()
+
+        return jsonify({"error": 'producto no encontrado'}), 404
+    
+    consulta_update = """
+        UPDATE productos
+        SET nombre = %s,
+            precio = %s,
+            stock = %s,
+        WHERE id = %s
+        RETURNING id, nombre, precio, stock;
+    """
+    cursor.execute(consulta_update,(nombre, precio, stock, producto_id))
+
+    fila_actualizada = cursor.fetchone()
+
+    conexion.commit()
+
+    cursor.close()
+    conexion.close()
+
+    return jsonify(fila_a_dict(fila_actualizada)), 200
+
+@apps.rout('/productos/<int:producto_id>', methods=['DELETE'])
+def eliminar_producto(producto_id):
+    conexion = get_connection()
+    cursor = conexion.cursor()
+
+    consulta_existe = 'SELECT id FROM productos WHERE id = %s'
+    cursor.execute(consulta_existe,(producto_id,))
+
+    fila = cursor.fetchone()
+
+    if fila is None:
+        cursor.close()
+        conexion.close()
+        return jsonify({'error':'producto no encontrado'}), 404
+    consulta_delete = 'DELETE FROM productos WHERE id = %s;'
+    cursor.execute(consulta_delete,(producto_id,))
+
+    conexion.commit()
+
+    cursor.close()
+    conexion.close()
+
+    return '', 204
 
 if __name__ == '__main__':
     apps.run(debug=True)
@@ -237,15 +172,3 @@ if __name__ == '__main__':
 
 
 ##################################3
-""" @app.route('/productos', methods=['POST'])
-def crear_producto():
-    datos = request.get_json()
-    
-
-
-
-
-if __name__ == "__main__":
-    app.run(debug=True) """
-
-
